37965b73dc5cd59c15a6d1bd7d89056d0b9e4773
Adarsh Gadekar <adarshgadekar@gmail.com>
2024-03-19 18:42:59 +0530

    added helloapiview

--- poc_api/views.py
+++ poc_api/views.py
@@ -1,1 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloApiView(APIView):
+    def get(self, request, format=None):
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you application logic',
+            'Is mapped manually to URLS'
+        ]
+
+        return Response({'message': 'hello!', 'an_apiview': an_apiview})
